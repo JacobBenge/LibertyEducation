a53507d07ed2b849704fccdf51d9d06143dbfaaf
JacobBenge <benge.jacob@gmail.com>
2021-02-10 10:35:37 -0700

    Added responsive feedback for password criteria on register page
    This helps the user choose a strong password, but you can still register with a weak password by deleting the "disabled" attribute from the register button.

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,23 +1,40 @@
-// https://jsbin.com/malopev/edit?html,js,output https://www.kurmis.com/2019/11/01/password-strength-javascript.html
+//THIS IS VULNERABLE. CAN REGISTER WITH WEAK PASSWORD BY DELETING THE DISABLED ATTRIBUTE FROM THE REGISTER BUTTON
 
-function passwordStrength(pw) {
-    return /.{8,}/.test(pw) * (  /* at least 8 characters */
+// https://jsbin.com/malopev/edit?html,js,output https://www.kurmis.com/2019/11/01/password-strength-javascript.html
+const pw = document.querySelector(".pwd")
+
+//TEST RETURNS THE VALUE OF 1 IF IT'S TRUE. IF ALL ARE TRUE IT RETURNS 4
+function passwordStrength(pwInput) {
+    return /.{8,}/.test(pwInput) * (  /* at least 8 characters */
         // /.{12,}/.test(pw)          /* bonus if longer */
-        + /[a-z]/.test(pw)         /* a lower letter */
-        + /[A-Z]/.test(pw)         /* a upper letter */
-        + /\d/.test(pw)            /* a digit */
-        + /[^A-Za-z0-9]/.test(pw)  /* a special character */
+        + /[a-z]/.test(pwInput)         /* a lower letter */
+        + /[A-Z]/.test(pwInput)         /* a upper letter */
+        + /\d/.test(pwInput)            /* a digit */
+        + /[^A-Za-z0-9]/.test(pwInput)  /* a special character */
     )
 };
 
-const pwInput = document.querySelector(".pwd")
-
-pwInput.addEventListener('keyup', function () {
-    document.getElementById("strength").value = passwordStrength(pwInput.value);
-    // WE WROTE THIS SECTION. IT TOGGLES THE DISABLED ATTRIBUTE FOR THE REGISTER BUTTON. THIS CAN EASILY BE BYPASSED BY MANUALLY EDITING HTML.
-    if (passwordStrength(pwInput.value) >= 4) {
-        document.querySelector(".register").removeAttribute("disabled");
-    } else {
-        document.querySelector(".register").setAttribute("disabled", true);
+// ACCEPTS THE CLASS NAME OF YOUR TARGET AND THE CRITERIA IT MUST MEET. IF CRITERIA IS MET, IT REMOVES THE UNMET CLASS.
+function checkPassReq(targClass, criteria) {
+    let target = document.querySelector(targClass);
+    if(criteria) {
+        target.classList.remove("unmet");
+    } else if (!target.classList.contains("unmet")) { // IF IT DOESN'T MEET THE CRITERIA AND DOESN'T CONTAIN UNMET, THEN ADD UNMET AGAIN.
+        target.classList.add("unmet");
     }
-});
+};
+
+// EVENT LISTENER FOR ANYTIME THE A KEY IS LET GO
+pw.addEventListener('keyup', function() {
+        if (passwordStrength(pw.value) >= 4) {
+            document.querySelector(".register").removeAttribute("disabled"); // SINCE WE MET ALL CRITERIA, ENABLE THE REGISTER BUTTON
+        } else {
+            document.querySelector(".register").setAttribute("disabled", true); // ENSURE THE DISABLED ATTRIBUTE ADDED SINCE WE DIDN'T MEET ALL CRITERIA
+        }
+        checkPassReq(".length", /.{8,}/.test(pw.value));
+        checkPassReq(".lower", /[a-z]/.test(pw.value));
+        checkPassReq(".upper", /[A-Z]/.test(pw.value));
+        checkPassReq(".number", /\d/.test(pw.value));
+        checkPassReq(".special", /[^A-Za-z0-9]/.test(pw.value));
+        checkPassReq(".bonus", /.{12,}/.test(pw.value));
+})
