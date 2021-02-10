19ee1156d490da53f8cd86243040dde258cab89b
JacobBenge <benge.jacob@gmail.com>
2021-02-10 02:03:39 -0700

    Added password strength validation to register
    This password strength checker can easily be bypassed by deleting the "disabled" attribute from the register button.

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -2,18 +2,22 @@
 
 function passwordStrength(pw) {
     return /.{8,}/.test(pw) * (  /* at least 8 characters */
-      /.{12,}/.test(pw)          /* bonus if longer */
-      + /[a-z]/.test(pw)         /* a lower letter */
-      + /[A-Z]/.test(pw)         /* a upper letter */
-      + /\d/.test(pw)            /* a digit */
-      + /[^A-Za-z0-9]/.test(pw)  /* a special character */
-     )
-  };
-  
-  const pwInput = document.querySelector(".pwd")
-  
-  pwInput.addEventListener('keyup', function() { 
-   document.getElementById("strength").value = 
-   passwordStrength(pwInput.value)
-  });
-  
+        // /.{12,}/.test(pw)          /* bonus if longer */
+        + /[a-z]/.test(pw)         /* a lower letter */
+        + /[A-Z]/.test(pw)         /* a upper letter */
+        + /\d/.test(pw)            /* a digit */
+        + /[^A-Za-z0-9]/.test(pw)  /* a special character */
+    )
+};
+
+const pwInput = document.querySelector(".pwd")
+
+pwInput.addEventListener('keyup', function () {
+    document.getElementById("strength").value = passwordStrength(pwInput.value);
+    // WE WROTE THIS SECTION. IT TOGGLES THE DISABLED ATTRIBUTE FOR THE REGISTER BUTTON. THIS CAN EASILY BE BYPASSED BY MANUALLY EDITING HTML.
+    if (passwordStrength(pwInput.value) >= 4) {
+        document.querySelector(".register").removeAttribute("disabled");
+    } else {
+        document.querySelector(".register").setAttribute("disabled", true);
+    }
+});
