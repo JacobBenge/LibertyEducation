f25e5a61044cd4a301644d29af930bf08b24cd83
JacobBenge <benge.jacob@gmail.com>
2021-02-20 17:20:54 -0700

    Added My Homework page in sportal
    A student can now view their assigned homework within the sportal

--- routes/sportal.js
+++ routes/sportal.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true }); // MERGE PARAMS ALLOWS THI
 const { isAuthenticated, isAdmin } = require('../middleware/middleware');
 const catchAsync = require('../utils/catchAsync');
 const Student = require('../models/student');
+const Homework = require('../models/homework');
 
 // ADDS req.flash(flashMessage) TO EVERY ROUTE. IF A flashMessage EXISTS, THEN IT WILL DISPLAY AT THE TOP OF THE PAGE
 router.get((req, res, next) => {
@@ -16,6 +17,32 @@ router.get('/', isAuthenticated, catchAsync(async (req, res) => {
         const userRegistrationCode = req.user.userRegistrationCode; // OBTAIN THE CODE FROM THE REQUEST
         const student = await Student.findById(userRegistrationCode) // LOOKUP THE MATCHING STUDENT
         res.render('sportal', { student }) // SEND THE STUDENT OBJECT BACK WITH THE RESPONSE SO ejs CAN ACCESS IT
+    } catch(e) {
+        req.flash('error', `Your registration code is no longer connected to a student profile. Admin's should create a dummy account to view the student portal`) // YOU GET THIS ERROR IF THE TEACHER DELETES A STUDENT FROM THE DATABASE AND THE USER TRIES TO VISIT THE STUDENT PORTAL.
+        return res.redirect('/');
+    }
+}))
+
+// RENDER THE STUDENT PROFILE PAGE
+router.get('/profile', isAuthenticated, catchAsync(async (req, res) => { 
+    try{
+        const userRegistrationCode = req.user.userRegistrationCode; // OBTAIN THE CODE FROM THE REQUEST
+        const student = await Student.findById(userRegistrationCode) // LOOKUP THE MATCHING STUDENT
+        res.render('sportal/profile', { student }) // SEND THE STUDENT OBJECT BACK WITH THE RESPONSE SO ejs CAN ACCESS IT
+    } catch(e) {
+        req.flash('error', `Your registration code is no longer connected to a student profile.`) // YOU GET THIS ERROR IF THE TEACHER DELETES A STUDENT FROM THE DATABASE AND THE USER TRIES TO VISIT THE STUDENT PORTAL.
+        return res.redirect('/');
+    }
+}))
+
+// RENDER THE HOMEWORK PAGE
+router.get('/homework', isAuthenticated, catchAsync(async (req, res) => { 
+    try{
+        const userRegistrationCode = req.user.userRegistrationCode; // OBTAIN THE CODE FROM THE REQUEST
+        const student = await Student.findById(userRegistrationCode) // LOOKUP THE MATCHING STUDENT
+        const homework = await Homework.find({ stuId: `${userRegistrationCode}`}).exec(); // LOOKUP THE MATCHING HOMEWORK https://mongoosejs.com/docs/api.html#model_Model.find
+        console.log(homework);
+        res.render('sportal/homework', { student, homework }) // SEND THESE OBJECTs BACK WITH THE RESPONSE SO ejs CAN ACCESS IT
     } catch(e) {
         req.flash('error', `Your registration code is no longer connected to a student profile.`) // YOU GET THIS ERROR IF THE TEACHER DELETES A STUDENT FROM THE DATABASE AND THE USER TRIES TO VISIT THE STUDENT PORTAL.
         return res.redirect('/');
